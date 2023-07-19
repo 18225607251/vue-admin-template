8660f1dc4e267800b338cf619f9d6086af1aa9ab
chubo <2569829371@qq.com>
2023-07-19 10:25:51 +0800

    first commit

--- src/views/nested/menu1/menu1-1/index.vue
+++ src/views/nested/menu1/menu1-1/index.vue
@@ -3,5 +3,21 @@
     <el-alert :closable="false" title="menu 1-1" type="success">
       <router-view />
     </el-alert>
+    <div class="father" />
   </div>
 </template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+   .father{
+     height: 300px;
+     width: 300px;
+     background-color: grey;
+   }
+
+</style>
