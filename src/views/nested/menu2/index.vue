e59e65c566b3c4b66b6d78b849cf669cdc2d159a
chubo <2569829371@qq.com>
2023-07-19 10:40:51 +0800

    "测试"

--- src/views/nested/menu2/index.vue
+++ src/views/nested/menu2/index.vue
@@ -1,5 +1,32 @@
 <template>
   <div style="padding:30px;">
     <el-alert :closable="false" title="menu 2" />
+    <div class="father">
+      <div class="son" />
+    </div>
   </div>
 </template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+   .father{
+     height: 300px;
+     width: 300px;
+     background: grey;
+     display: flex;
+     justify-content: center;
+     align-items: center;
+     .son{
+      height: 100px;
+      width: 100px;
+      background-color: red;
+      // align-self: flex-start;
+     }
+   }
+
+</style>
